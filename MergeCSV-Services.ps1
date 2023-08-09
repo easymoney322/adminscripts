f6ded534e7133029ec0a8f1917940328bbbbeb5c
Mikhail Samodurov <20065717+easymoney322@users.noreply.github.com>
2023-08-09 11:13:30 +0500

    Added MergeCSV-Services.ps1
    This script is used for merging two CSVs you get VIA
    ```
     Get-Service | Select-Object -Property Name,@{n='DisplayNameRU';e={$_.DisplayName}} | Export-Csv -Encoding UTF8 ./SrvcsRU.csv    
     Get-Service | Select-Object -Property Name,@{n='DisplayNameEN';e={$_.DisplayName}} | Export-Csv -Encoding UTF8 ./SrvcsEN.csv  
    ```
    Since service name depends on a system language of a user, for the script to make a sense you must generate these CSV files on a different system languages, then join them together with this script.

---
+++ MergeCSV-Services.ps1
@@ -0,0 +1,39 @@
+﻿clear
+$dirpath = ($env:HOMEPATH + "\Desktop\");
+$Filename_RU = "SrvcsRU.csv";
+$Filename_EN = "SrvcsEN.csv";
+
+$CSV_RU = Import-CSV $($dir + $Filename_RU) -Encoding UTF8;
+$CSV_EN = Import-CSV $($dir + $Filename_EN) -Encoding UTF8;
+$arrayofobj = @();
+for ($i =0; $i -lt $CSV_RU.Count; $i++)
+{
+    $k =0; $found=$false;
+    While (($k -lt $CSV_EN.Count) -and (!$found))
+    {
+      While (($CSV_EN[$k].Name -le $CSV_RU[$i].Name) -and ( $k -lt $CSV_EN.Count))
+      {
+         if ($CSV_RU[$i].Name -eq $CSV_EN[$k].Name)
+         {
+            $arrayofobj += [PSCustomObject]@{
+                'Name' = $CSV_EN[$k].Name
+                'RU' = $CSV_RU[$i].DisplayNameRU
+                'EN' = $CSV_EN[$k].DisplayNameEN;
+             } 
+           $found=$true;
+           break;
+         }
+         $k++
+      }
+      $k++
+    }
+    if (!$found)
+    {
+        $arrayofobj += [PSCustomObject]@{
+        'Name' = $CSV_RU[$i].Name
+        'RU' = $CSV_RU[$i].DisplayNameRU
+        'EN' = ''
+        }
+    }
+}
+$arrayofobj| Export-Csv -Path $($dir+"Out.csv") -Encoding UTF8 -Delimiter ';' -NoTypeInformation
