2cb8dc26a91c846c3f3dff0a5d7bae94dd3e98eb
Mikhail Samodurov <20065717+easymoney322@users.noreply.github.com>
2023-08-25 11:59:16 +0500

    Added DownloadOHM.ps1
    This scripts downloads OHM, creates a task for it in task scheduler, and launches it.

---
+++ DownloadOHM.ps1
@@ -0,0 +1,11 @@
+﻿$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8");
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8';
+$DLDS = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path;
+Invoke-WebRequest -Uri 'https://openhardwaremonitor.org/files/openhardwaremonitor-v0.9.6.zip' -OutFile $DLDS;
+Expand-Archive ($DLDS+ "ohm.zip")  -DestinationPath 'C:\Program Files\'
+$scheduleObject = New-Object -ComObject schedule.service;
+$scheduleObject.connect();
+$rootFolder = $scheduleObject.GetFolder("\");
+$rootFolder.CreateFolder("Open Hardware Monitor");
+Register-ScheduledTask -TaskName "Startup" -Action $(New-ScheduledTaskAction -Execute 'C:\Program Files\OpenHardwareMonitor\OpenHardwareMonitor.exe') -TaskPath "\Open Hardware Monitor\" -RunLevel Highest -User "System" -Trigger $(New-ScheduledTaskTrigger -AtStartup -RandomDelay (New-TimeSpan -minutes 3));
+Start-ScheduledTask -TaskPath "\Open Hardware Monitor" -TaskName "Startup";
