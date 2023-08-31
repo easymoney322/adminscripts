bcfdf94086f51c340cb40f62388b1a246e1a5504
Mikhail Samodurov <20065717+easymoney322@users.noreply.github.com>
2023-08-31 10:43:06 +0500

    Update winrm-enable.ps1
    Improved idempotency and compatibility when executed straight from powershell.

--- winrm-enable.ps1
+++ winrm-enable.ps1
@@ -1,28 +1,87 @@
-try {
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope Process
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope LocalMachine
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope CurrentUser
-}
-catch
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8");
+Clear-Host;
+
+try 
 {
-    echo "Error occured during the change of execution policy"
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope Process;
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope LocalMachine;
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope CurrentUser;
+}catch
+{
+    echo "Error occured during the change of execution policy";
 }
 
-New-NetFirewallRule -DisplayName NewFromADM -Action Allow -RemoteAddress @("192.168.100.73", "192.168.100.24", "192.168.100.1") -Direction Inbound -Enabled "True";    
-New-NetFirewallRule -DisplayName NewFromADM -Action Allow -RemoteAddress @("192.168.100.73", "192.168.100.24", "192.168.100.1") -Direction Outbound -Enabled "True";
 
-winrm quickconfig -quiet
-[System.Threading.Thread]::Sleep(2300)
-Enable-PSRemoting –Force
+$whitelist = @("192.168.100.73", "192.168.100.24", "192.168.100.1", "192.168.0.24", "192.168.100.43");
+$firewallRules = @();
+$firewallRulesInbound = @();
+$firewallRulesOutbound = @();
+$rulesExist = $true;
+$ruleInboundExists = $true;
+$ruleOutboundExists = $true;
+
+try
+{
+    $firewallRules = Get-NetFirewallrule -DisplayName 'NewFromADM' -ErrorAction Stop;
+}catch  [Microsoft.PowerShell.Cmdletization.Cim.CimJobException] 
+{
+    if ($_.Exception.Message -imatch ".*DisplayName.*")
+    {
+        $rulesExist = $false;
+    }
+}
+
+
+if ($rulesExist -eq $true)
+{
+  for ($i = 0; $i -lt $firewallRules.Count; $i++)
+  {
+    if ($firewallRules[$i].Direction.toString() -eq "Inbound")
+    {
+        $firewallRulesInbound += $firewallRules[$i];
+        $ruleInboundExists = $true;
+    }else
+    {
+        $firewallRulesOutbound += $firewallRules[$i];
+        $ruleOutboundExists = $true;
+    }
+  }
+}else 
+{
+    New-NetFirewallRule -DisplayName NewFromADM -Action Allow -RemoteAddress $whitelist -Direction Inbound -Enabled "True";  
+    New-NetFirewallRule -DisplayName NewFromADM -Action Allow -RemoteAddress $whitelist -Direction Outbound -Enabled "True";
+    Exit;
+}
+
+if ($ruleInboundExists -eq $false)
+{
+    New-NetFirewallRule -DisplayName NewFromADM -Action Allow -RemoteAddress $whitelist -Direction Inbound -Enabled "True";   
+}else
+{
+    $firewallRulesInbound | Set-NetFirewallRule -Action Allow -RemoteAddress $whitelist -Enabled "True";
+}
+
+if ($ruleOutboundExists -eq $false)
+{
+    New-NetFirewallRule -DisplayName NewFromADM -Action Allow -RemoteAddress $whitelist -Direction Outbound -Enabled "True";
+}else 
+{
+    $firewallRulesOutbound | Set-NetFirewallRule -Action Allow -RemoteAddress $whitelist -Enabled "True";
+}
+
+
+
+winrm quickconfig -quiet;
+[System.Threading.Thread]::Sleep(2300);
+Enable-PSRemoting â€“Force;
 [System.ServiceProcess.ServiceController] $SC = Get-Service -Name WinRM;
-[string]$ST = $SC.StartType
+[string]$ST = $SC.StartType;
 
 if (!("Automatic".Equals($ST)))
 {
-    Set-Service -Name WinRM -StartupType Automatic
-}
-else
+    Set-Service -Name WinRM -StartupType Automatic;
+}else
 {
-    echo "WinRM is already set to Automatic"
+    echo "WinRM is already set to Automatic";
 }
 Set-PSSessionConfiguration -ShowSecurityDescriptorUI -Name Microsoft.PowerShell;
