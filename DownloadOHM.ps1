6844bbba38127cc9c54eb50d8f6b9cf9b7b87156
Mikhail Samodurov <20065717+easymoney322@users.noreply.github.com>
2023-09-05 10:32:38 +0500

    Update DownloadOHM.ps1

--- DownloadOHM.ps1
+++ DownloadOHM.ps1
@@ -10,7 +10,7 @@ if ($dlfold.Attributes -imatch ".*ReadOnly.*" )
     $dlfold.Attributes = $dlfold.Attributes -band -bnot [System.IO.FileAttributes]::ReadOnly;
     
 } 
-                               
+                   
 $DLDS += "/ohm.zip"
 Invoke-WebRequest -Uri 'https://openhardwaremonitor.org/files/openhardwaremonitor-v0.9.6.zip' -OutFile $DLDS;
 Expand-Archive ($DLDS)  -DestinationPath 'C:\Program Files\';
@@ -24,5 +24,5 @@ $scheduleObject = New-Object -ComObject schedule.service;
 $scheduleObject.connect();
 $rootFolder = $scheduleObject.GetFolder("\");
 $rootFolder.CreateFolder("Open Hardware Monitor");
-Register-ScheduledTask -TaskName "Startup" -Action $(New-ScheduledTaskAction -Execute 'C:\Program Files\OpenHardwareMonitor\OpenHardwareMonitor.exe') -TaskPath "\Open Hardware Monitor\" -RunLevel Highest -User "System" -Trigger $(New-ScheduledTaskTrigger -AtStartup -RandomDelay (New-TimeSpan -minutes 3));
+Register-ScheduledTask -TaskName "Startup" -Action $(New-ScheduledTaskAction -Execute 'C:\Program Files\OpenHardwareMonitor\OpenHardwareMonitor.exe') -TaskPath "\Open Hardware Monitor\" -RunLevel Highest -User "System" -Trigger $(New-ScheduledTaskTrigger -Settings $(New-ScheduledTaskSettingsSet -ExecutionTimeLimit 0) -AtStartup -RandomDelay (New-TimeSpan -minutes 3));
 Start-ScheduledTask -TaskPath "\Open Hardware Monitor" -TaskName "Startup";
